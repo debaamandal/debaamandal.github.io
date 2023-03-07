00d619e70395dcc40c9c9fcf771bffd8c04c4658
Debaa Mandal <debaawtf@gmail.com>
2023-03-07 09:42:24 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+### Hi there, I'm Debaa Mandal and I made this [website]. 👋
+
+### Connect with me:
+
+[<img align="left" alt="Debaa | Instagram" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/instagram.svg" />][instagram]
+[<img align="left" alt="Debaa | Twitter" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/twitter.svg" />][twitter]
+[<img align="left" alt="Debaa | YouTube" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/youtube.svg" />][youtube]
+[<img align="left" alt="debaa.online" width="22px" src="https://raw.githubusercontent.com/iconic/open-iconic/master/svg/globe.svg" />][website]
+
+<br />
+
+[instagram]: https://instagram.com/devcmon/
+[twitter]: https://twitter.com/devcmon
+[youtube]: https://www.youtube.com/@devcmon
+[website]: https://debaamandal.github.io/
---
+++ dist/js/scripts.js
@@ -0,0 +1,76 @@
+/*!
+* Start Bootstrap - Resume v7.0.1 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+// preloader 
+
+var loader = document.querySelector(".preloader")
+
+window.addEventListener("load", vanish);
+
+function vanish() {
+    loader.classList.add("disppear");
+}
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+// Birthday
+const countdown = () => {
+    const countDate = new Date("August 17, 2023 00:00:00").getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    // Time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Calculate
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour)
+    const textMinute = Math.floor((gap % hour) / minute)
+    const textSecond = Math.floor((gap % minute) / second)
+    
+    // Replacing text
+    document.querySelector(".day").innerText = textDay;
+    document.querySelector(".hour").innerText = textHour;
+    document.querySelector(".minute").innerText = textMinute;
+    document.querySelector(".second").innerText = textSecond;
+}
+
+setInterval(countdown, 1000);
+
+// Copyright
+const yearSpan = document.querySelector('#currentYear');
+const currentYear = new Date();
+yearSpan.innerText = currentYear.getFullYear();
